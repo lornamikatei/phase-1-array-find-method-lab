e84616edad0dc2e6fd1ed8a88db6cc7eeb315969
Mringa School <lorna.mikatei@student.moringaschool.com>
2022-10-05 14:35:54 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    
+]
+function superbowlWin(teamRecord){
+    const winElement =teamRecord.find(compareWin)
+
+    if(winElement !== undefined){
+        return winElement.year
+    }
+    else {
+        return winElement
+    }
+
+}
+
+function compareWin(element){
+    return element.result === "W"
+}
